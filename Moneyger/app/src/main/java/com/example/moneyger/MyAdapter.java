1ac84e2f9fd4cf2299467a4d58e7cece9032b33c
softone-as <shofwanas12@gmail.com>
2021-04-07 10:58:13 +0700

    Added Task 3 - Fragment

---
+++ Moneyger/app/src/main/java/com/example/moneyger/MyAdapter.java
@@ -0,0 +1,57 @@
+package com.example.moneyger;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    String[] s1;
+    String[] s2;
+    int[] images;
+    Context context;
+    public MyAdapter(Context context, String[] s1, String[] s2, int[] images){
+        this.context = context;
+        this.s1 = s1;
+        this.s2 = s2;
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.myText1.setText(s1[position]);
+        holder.myText2.setText(s2[position]);
+        holder.myImage.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView myText1, myText2;
+        ImageView myImage;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myText1 = itemView.findViewById(R.id.textView1);
+            myText2 = itemView.findViewById(R.id.textView2);
+            myImage = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
